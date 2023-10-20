4b9fc8bb3216986bbe5df3a0f68edd8d2c4629b9
Mihir Gupta <guptamihir@protonmail.com>
2023-10-20 17:29:44 -0400

    Initial Commit

---
+++ Fetch_iOS_Coding_Challenge/Fetch_iOS_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_Coding_ChallengeApp.swift
+//  Fetch_iOS_Coding_Challenge
+//
+//  Created by Mihir Gupta on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
