df459616e2a868e6b62a8307403d173c94f23194
Mihir Gupta <guptamihir@protonmail.com>
2023-10-22 15:59:23 -0400

    can read from api easily

--- Fetch_iOS_Coding_Challenge/ContentView.swift
+++ Fetch_iOS_Coding_Challenge/ContentView.swift
@@ -7,18 +7,19 @@
 
 import SwiftUI
 
-struct Response : Codable{
+struct Response : Hashable, Codable{
     let meals : [MyMeal]
 }
-struct MyMeal :  Codable {
+struct MyMeal :  Hashable, Codable {
     var strMeal : String
     var strMealThumb : String
     var idMeal : String
     
 }
 
-
+var json : Response?
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
     @State private var myMeals = [MyMeal]()
     var body: some View {
         NavigationView {
@@ -36,7 +37,7 @@ struct ContentView: View {
             .navigationTitle("Meals")
             .task {
                // await fetchData()
-                getData()
+                viewModel.getData()
                 
             }
         }
@@ -44,35 +45,54 @@ struct ContentView: View {
     
     //let url = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
     
-    
-    func getData(){
-        guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else{
-            print("URL doesn't work")
-            return
+    class ViewModel : ObservableObject {
+        @Published var responses : [Response] = []
+        @Published var meals : [MyMeal] = []
+        
+        func getData() {
+            
+            guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")
+            else {
+                print("URL doesn't work")
+                return
+            }
+            let task = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+                guard let data = data, error == nil else{
+                    print("Something went wrong")
+                    return
+                }
+                
+                //now I have the data
+                var desserts : Response?
+                do {
+                    desserts = try JSONDecoder().decode(Response.self, from: data)
+                   //print(desserts!)
+                    //print(desserts!.meals)
+                    
+                   // print(self.responses.first?.meals.first?.strMeal as Any)
+                    //self.meals = desserts!.meals
+                    DispatchQueue.main.async {
+                        self.meals = desserts!.meals
+                    }
+                    self.meals = desserts!.meals
+                    print(self.meals)
+                }
+                catch{
+                    print(String(describing:error))
+                }
+                
+                guard let json = desserts else{
+                    return
+                }
+                
+                //print(json.meals.first?.strMeal)
+                //print(json.meals.first?.idMeal)
+                
+            })
+            task.resume()
         }
-        let task = URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
-            guard let data = data, error == nil else{
-                print("Something went wrong")
-                return
-            }
-            
-            //now I have the data
-            var meals : Response?
-            do{
-                meals = try JSONDecoder().decode(Response.self, from: data)
-            }
-            catch{
-                print(String(describing:error))
-            }
-            guard let json = meals else{
-                return
-            }
-            
-            print(json.meals.first?.strMeal)
-            //print(json.meals.idMeal)
-        })
-        task.resume()
     }
+    
     
     func fetchData() async{
         //create url
