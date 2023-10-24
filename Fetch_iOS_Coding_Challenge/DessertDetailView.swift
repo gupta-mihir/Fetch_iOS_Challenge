ae11a8480758c360cd6cdc830b071f5964e6d804
Mihir Gupta <guptamihir@protonmail.com>
2023-10-23 21:11:37 -0400

    displays ingredients and measurements

--- Fetch_iOS_Coding_Challenge/DessertDetailView.swift
+++ Fetch_iOS_Coding_Challenge/DessertDetailView.swift
@@ -36,20 +36,97 @@ struct DessertDetailView: View {
                         
                         
                         
-                        Text(meal_2.strMeal)
-                            .multilineTextAlignment(.leading)
-                            .bold()
-                            .padding(5.0)
-                            .background(Color.white)
-                            .cornerRadius(13)
+                        
                         
                     }
                     Text(meal_2.strInstructions)
+                    Text("Ingredients")
+                        .font(.title2)
+                    HStack{
+                        Text(meal_2.strMeasure1)
+                        Text(meal_2.strIngredient1)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure2)
+                        Text(meal_2.strIngredient2)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure3)
+                        Text(meal_2.strIngredient3)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure4)
+                        Text(meal_2.strIngredient4)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure5)
+                        Text(meal_2.strIngredient5)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure6)
+                        Text(meal_2.strIngredient6)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure7)
+                        Text(meal_2.strIngredient7)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure8)
+                        Text(meal_2.strIngredient8)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure9)
+                        Text(meal_2.strIngredient9)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure10)
+                        Text(meal_2.strIngredient10)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure11)
+                        Text(meal_2.strIngredient11)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure12)
+                        Text(meal_2.strIngredient12)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure13)
+                        Text(meal_2.strIngredient13)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure14)
+                        Text(meal_2.strIngredient14)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure15)
+                        Text(meal_2.strIngredient15)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure16)
+                        Text(meal_2.strIngredient16)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure17)
+                        Text(meal_2.strIngredient17)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure18)
+                        Text(meal_2.strIngredient18)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure19)
+                        Text(meal_2.strIngredient19)
+                    }
+                    HStack{
+                        Text(meal_2.strMeasure20)
+                        Text(meal_2.strIngredient20)
+                    }
                     
                 }
             }
             }
-            .navigationTitle("Desserts")
+            .navigationTitle(meals.strMeal)
             .task {
                 
                 detailViewModel.getData(id: meals.idMeal)
